367d336109b14c4c2cd2b60c99b5245ca5b417e2
tejaAPI <brahmateja.bollineni@gmail.com>
2017-10-07 18:10:14 +0530

    second commit

--- Project.java
+++ Project.java
@@ -5,18 +5,70 @@ public class Project
 	public static void main(String args[])
 	{
 		Scanner s=new Scanner(System.in);
-		int n;
-		System.out.println("\nExperiment-1");
-		System.out.println("\nExperiment-2");
-		System.out.println("\nExperiment-3");
-		System.out.println("\nExperiment-4");
-		System.out.println("\nExperiment-5");
-		System.out.println("\nExperiment-6");
-		System.out.println("\nExperiment-7");
-		System.out.println("\nExperiment-8");
-		System.out.println("\nExperiment-9");
-		System.out.println("\nExperiment-10");
-		System.out.println("\nExperiment-11"); 
+		int n,f=1,ch;
+		char c;
+		while(f==1)
+		{
+		System.out.println("**DEPATMENT OF IT WELCOMES YOU**");
+		System.out.println("************MENU****************");
+		System.out.println("DO YOU WANT BASIC PROGRAMS OF JAVA OR LAB EXPERIMENTS:");
+		System.out.println("BASIC PROGRAMS - PRESS 1");
+		System.out.println("LAB EXPERIMENTS - PRESS 2");
+		System.out.println("ENTER YOUR CHOICE:");
+		ch=s.nextInt();
+		if(ch==1)
+		{
+		System.out.println("*****BASIC PROGRAMS*******");
+		System.out.println("1.HELLO WORLD");
+		System.out.println("2.AREA OF THE CIRCLE");
+		System.out.println("3.GRETEST AMONF THREE NUMBERS");
+		System.out.println("4.CREATING A BOX OBJECT AND FINDING VOLUME");
+		System.out.println("5.INHERITANCE");
+		System.out.println("6.USING SUPER KEYWORD");
+		System.out.println("enter your choice:");
+		n=s.nextInt();
+		if(n>6)
+		{
+			System.out.println("enter correct choice:");
+			n=s.nextInt();
+		}
+			switch(n)
+		{
+			case 1: example exam=new example();
+					exam.hello();
+					break;
+			case 2: circle cr=new circle();
+					cr.circlearea();
+					break;
+			case 3: teja tj=new teja();
+					tj.bas();
+					break;
+
+			case 4: greatestnumber gn=new greatestnumber();
+					gn.great();
+					break;
+			case 5: objin k=new objin();
+					k.inher();
+					break;
+			case 6: supe ep=new supe();
+					ep.sup();
+					break;
+		}
+		}
+		
+		if(ch==2)
+		{
+		System.out.println("\nExp-1:BASIC ARITHMETIC OPERATIONS");
+		System.out.println("\nExp-2:WHETHER A GIVEN STRING IS A PALINDROME OR NOT");
+		System.out.println("\nExp-3:SET OF PRIME NUMBERS BETWEEN TWO GIVEN RANGES");
+		System.out.println("\nExp-4:SORTING A GIVEN LIST OF NAMES");
+		System.out.println("\nExp-5:USING STRING TOKENIZER CLASS");
+		System.out.println("\nExp-6:FOR CREATING MULTIPLE THREADS USING THREAD CLASS");
+		System.out.println("\nExp-7:FOR CREATING MULTIPLE THREADS USING RUNNABLE INTERFACE");
+		System.out.println("\nExp-8: HOW RUN TIME POLYMORPHISM IS ACHIEVED");
+		System.out.println("\nExp-9:IMPLEMENTS INTERFACES CONCEPT");
+		System.out.println("\nExp-10:HANDLING PREDEFINED EXCEPTIONS");
+		System.out.println("\nExp-11:HANDLING USER DEFINED EXCEPTIONS"); 
 		System.out.println("\nenter your choice:");
 		n=s.nextInt();
 		if(n>11)
@@ -26,86 +78,69 @@ public class Project
 		}
 		switch(n)
 		{
-			case 1: arith ar=new arith();
-					ar.basicoper();
+			case 1: Arithmetic ar=new Arithmetic();
+					ar.basicoper1();
 					break;
-			case 2: ariths a=new ariths();
-					a.basicoper1();
+			case 2: palindrome p=new palindrome();
+					p.pal();
 					break;
-			case 3: circle c=new circle();
-					c.circlearea();
+			case 3: prime1 pr=new prime1();
+					pr.primerange();
 					break;
 
-			case 4: example exa=new example();
-					exa.hello();
-					break;
-			case 5: exce e=new exce();
-					e.except();
-					break;
-			case 6: exp3 ex=new exp3();
+			case 4: exp3 ex=new exp3();
 					ex.stringsort();
 					break;
-			case 7: Interface i=new Interface();
-					i.interfac();
+			case 5: StringToken st=new  StringToken();
+					st.stringtok();
 					break;
-
-			case 8: Multiple_Thread1 mu=new Multiple_Thread1();
+			case 6: Multiple_Thread1 mu=new Multiple_Thread1();
 					mu.thred();
 					break;
-			case 9: Multiple_Thread2 m=new Multiple_Thread2();
+			case 7: Multiple_Thread2 m=new Multiple_Thread2();
 					m.thred1();
 					break;
-			case 10: Newexce nex=new Newexce();
-						nex.ex();
+			case 8:Splender sp=new Splender();
+					sp.sb();
+					break;
+			
+
+			case 9: Interface i=new Interface();
+					i.interfac();
+					break;
+			
+			case 10: predefexc e=new predefexc();
+					e.exc();
 					break;
 			
 			
-			case 11: 
-					prime1 p=new prime1();
-					p.primerange();
+			case 11: Newexce ne=new Newexce();
+					ne.ex();
+					
 					break;
-			case 12:
-			 StringToken st=new  StringToken();
-			 st.stringtok();
-			 break;
 			
+			
+		}
+		}
+		System.out.println("do you want to choose again:(y/n):");
+		c=s.next().charAt(0);
+		if(c=='y')
+			f=1;
+		else
+			f=0;
+		}
 }
 }
- }
 	
-		
-
-
-
-
-
-
-
-
-
-
-
-
-class arith
-{
-	void basicoper()
-	{
-		int a=20,b=10;
-
-		System.out.println("the addition of two numbers is "+(a+b));
-		System.out.println("the difference between two numbers is "+(a-b));
-		System.out.println("the division of two numbers is "+(a/b));
-		System.out.println("remainder is "+(a%b));
-		System.out.println("the product of two numbers is "+(a*b));
-	}
-}
-class ariths
+	
+class Arithmetic
 {
 	void basicoper1()
 	{
 		Scanner s=new Scanner(System.in);
 
 		int a,b;
+		System.out.println("Enter two numbers:");
 		a=s.nextInt();
 		b=s.nextInt();
 		
@@ -117,60 +152,49 @@ class ariths
 		System.out.println("product= "+(a*b));
 	}
 }
-
-
-class circle
+class palindrome
 {
-	
-	void circlearea()
+void pal()
+{        
+ Scanner s=new Scanner(System.in);
+System.out.print("Enter a String: ");
+                 String s1=s.next();
+                 String s2=new String();
+int n=s1.length();
+for(int i=n-1;i>=0;i--)
+               s2=s2+ s1.charAt(i);
+if(s1.equals(s2)==true)
+System.out.println("String is Palindrome");
+else
+System.out.println("string is not a Palindrome");
+}
+}
+class prime1
+{
+	void primerange()
 	{
 		Scanner s=new Scanner(System.in);
-		int r;
-		System.out.println("Enter the radius of the circle");
-		r=s.nextInt();		
-		double a=Math.PI*(r*r);
-		double c=Math.PI*2*r;
-		System.out.println("the area of the circle is "+a);
-		System.out.println("the circumference of the circle is "+c);
+		int f,e;
+		System.out.println("Enter the limits");
+		System.out.println("Enter the lower limit");
+		f=s.nextInt();
+		System.out.println("Enter the upper limit");
+		e=s.nextInt();
+		System.out.println("The prime numbers are ");
+		for(int n=f;n<=e;n++)
+		{
+			int count=0;
+			for(int i=1;i<=n;i++)
+			{
+				if(n%i==0)
+				count++;
+			}
+			if(count==2)
+				System.out.println("   "+n);
+		}
 	}
 }
-		
-		
-		
-class example 
-{
-	void hello()
-	{
-		System.out.println("HELLO WORLD");
-	}
-}
-
-class exce
-{
-   void except()
-    {
-        try
-        {
-			Scanner s=new Scanner(System.in);
-            System.out.println("Enter the numerator");
-			int a=s.nextInt();
-			System.out.println("Enter the denominator");
-			int b=s.nextInt();
-            float c = a/b;
-			System.out.println("The division is "+c);
-         }
-        catch(ArithmeticException e)
-        {
-            System.out.println("Division by zero");
-        }
-        finally
-        {
-            System.out.printf("Finally is always executed");
-        }
-    }
-}
-
- class exp3 
+class exp3 
 {
 	void stringsort()
 	{
@@ -202,28 +226,27 @@ class exce
 			System.out.println("  "+arr[i]);
 	}
 }
-		
-		
-		
-interface A
+class StringToken
 {
-    int x=10;
+	void stringtok()
+	{
+  		String str;
+		int sum=0;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the string");
+		str=s.nextLine();
+		StringTokenizer a=new StringTokenizer(str," ",false);
+		System.out.println("integers are ");
+		while(a.hasMoreTokens())
+    		{
+			int b=Integer.parseInt(a.nextToken());
+			sum=sum+b;
+			System.out.println(" "+b);
+			
+     		}
+		System.out.println("sum of integers is "+sum);
+	}
 }
-interface B
-{
-    int x=100;
-}
-class Interface implements A,B
-{
-    void interfac()
-    {
-      
-       //System.out.println(x); 
-       System.out.println(A.x);
-       System.out.println(B.x);
-    }
-}
-
 class Ab extends Thread
 
 {
@@ -242,9 +265,8 @@ class Ab extends Thread
 		{
 		}
 	}
- 
+	
 }
-
 class Bc extends Thread
 {
 	
@@ -276,7 +298,6 @@ class Multiple_Thread1
 		t2.start();
 	}
 }
-
 class C implements Runnable
 {
 	public void run()
@@ -326,9 +347,67 @@ class Multiple_Thread2
 		t2.start();
 	}
 }
+//runtime polymorphism
+
+class Bike{  
+ void run(){System.out.println("running");}  
+}  
+class Splender extends Bike{  
+  void run(){System.out.println("running safely with 60km");}  
+  
+  void sb(){  
+    Bike b = new Splender();//upcasting  
+    b.run();  
+  }  
+}  
+  
+
+interface A
+{
+    int x=10;
+}
+interface B
+{
+    int x=100;
+}
+class Interface implements A,B
+{
+    void interfac()
+    {
+      
+       //System.out.println(x); 
+       System.out.println(A.x);
+       System.out.println(B.x);
+    }
+}
 
 
- class Newexce
+ 
+
+class predefexc
+{
+	void exc()
+	{
+		try
+		{
+		int a=0;
+		System.out.println("a="+a);
+		int b=42/a;
+		int c[]={1};
+		c[42]=998;
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println(e);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+	}
+	
+}	
+class Newexce
 {
 	void ex()
 	{
@@ -339,7 +418,7 @@ class Multiple_Thread2
 			{
 				throw new excep();
 			}
-			int c=10/0;
+			int c=10/a;
 			System.out.println("c");
 		}
 		catch(excep e)
@@ -356,54 +435,171 @@ class excep extends Exception
 		System.out.println("Divided by zero");
 	}
 }
-class palindromen
+
+class box1
 {
-	void numpalin()
+	double length=2;
+	double height=3;
+	double width=3;
+	box1()
+	{
+		length=length;
+		width=width;
+		height=height;
+	}
+	void volume()
+	{
+		double vol=length*height*width;
+		System.out.println("volume="+vol);
+		
+	}
+	void display()
+	{
+		System.out.println("length="+length+","+"width="+width+","+"height="+height);
+	}
+	
+
+}
+class teja
+{
+	void bas()
+	{
+		box1 ob=new box1();
+		
+		box1 od=new box1();
+			
+		ob.volume();
+		od.volume();
+		ob.display();
+		od.display();
+		
+	}
+}
+class circle
+{
+	
+	void circlearea()
 	{
 		Scanner s=new Scanner(System.in);
-		int n,temp,rn=0,d;
-		System.out.println("enter the number");
-		n=s.nextInt();
-		temp=n;
-		while(n>0)
-		{	
-			d=n%10;
-			rn=rn*10+d;
-			n=n/10;
-		}
-		if(rn==(temp))
-			System.out.println(" the given number is palindrome");
-		else
-			System.out.println(" the given number is not palindrome");
+		int r;
+		System.out.println("Enter the radius of the circle");
+		r=s.nextInt();		
+		double a=Math.PI*(r*r);
+		double c=Math.PI*2*r;
+		System.out.println("the area of the circle is "+a);
+		System.out.println("the circumference of the circle is "+c);
+	}
+}
+		
+		
+		
+class example 
+{
+	void hello()
+	{
+		System.out.println("HELLO WORLD");
+	}
+}
+ 
+class greatestnumber
+{
+ void great()
+ {
+int a,b,c,largest;
+Scanner s=new Scanner(System.in);
+System.out.println("Enter any three numbers: ");
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+
+if(a>=b && a>=c) 
+{
+System.out.println("Largest number: "+a);
+}
+if(b>=a && b>=c) 
+{
+System.out.println("Largest number: "+b);
+}
+if(c>=a && c>=b) 
+{
+System.out.println("Largest number: "+c);
+}
+}
+}		
+		
+		
+class box
+{
+	double width;
+	double height;
+	double depth;
+	box(double w,double h,double d)
+	{
+		width=w;
+		height=h;
+		depth=d;
+	}
+}
+class boxweight extends box
+{
+	double weight;
+	boxweight(double w,double h,double d,double m)
+	{
+		super(w,h,d);
+		weight=m;
+	}
+	double vol()
+	{
+		return width*height*depth;
+	}
+	
+}
+class supe{
+	void sup()
+	{
+		boxweight b=new boxweight(1,2,3,4);
+		double a=b.vol();
+		System.out.println("volume is:"+a);
+	}
+}	
+
+class ina
+{
+	int i;
+	int j;
+	void setdim(int x,int y)
+	{
+		i=x;
+		j=y;
+	}
+}
+class inb extends ina
+{
+	int total;
+	void sum()
+	{
+		System.out.println( i+","+j );
+		total=i+j;
+	}
+}
+class objin
+{
+	void inher()
+	{
+		ina sup=new ina();
+		inb sob=new inb();
+		sob.setdim(2,3);
+		sob.sum();
+		System.out.println("sum="+sob.total);
 	}
 }
 
 
-class prime1
-{
-	void primerange()
-	{
-		Scanner s=new Scanner(System.in);
-		int f,e;
-		System.out.println("Enter the limits");
-		System.out.println("Enter the lower limit");
-		f=s.nextInt();
-		System.out.println("Enter the upper limit");
-		e=s.nextInt();
-		System.out.println("The prime numbers are ");
-		for(int n=f;n<=e;n++)
-		{
-			int count=0;
-			for(int i=1;i<=n;i++)
-			{
-				if(n%i==0)
-				count++;
-			}
-			if(count==2)
-				System.out.println("   "+n);
-		}
-	}
-}
+
+
+
+ 
+
 
 	
 		
@@ -413,27 +609,7 @@ class prime1
 
 
 
-class StringToken
-{
-	void stringtok()
-	{
-  		String str;
-		int sum=0;
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the string");
-		str=s.nextLine();
-		StringTokenizer a=new StringTokenizer(str," ",false);
-		System.out.println("integers are ");
-		while(a.hasMoreTokens())
-    		{
-			int b=Integer.parseInt(a.nextToken());
-			sum=sum+b;
-			System.out.println(" "+b);
-			
-     		}
-		System.out.println("sum of integers is "+sum);
-	}
-}
+
 
 
 
